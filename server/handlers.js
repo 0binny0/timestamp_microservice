4346eca419226d06eb37bceebaa9437c47a61fe6
0binny0 <binnyhh@protonmail.com>
2024-06-25 12:55:31 -0700

    removed res.send(Access-Control-Allow-Origin)

--- server/handlers.js
+++ server/handlers.js
@@ -2,7 +2,6 @@
 import {validate_date} from "./validators.js";
 
 function get_timestamps(req, res) {
-    res.send("Access-Control-Allow-Origin", "*");
     res.json(validate_date(req.params.date));
 }
 

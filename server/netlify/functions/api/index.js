4ea158bd70b904d31731479dd3651d33e13ca86f
0binny0 <binnyhh@protonmail.com>
2024-06-24 22:54:45 -0700

    installed cors

--- server/netlify/functions/api/index.js
+++ server/netlify/functions/api/index.js
@@ -1,10 +1,12 @@
 
 import express from "express";
-import {dates_router} from "./date.js";
+import {cors} from "cors";
 import serverless from "serverless-http";
 
+import {dates_router} from "./date.js";
+
 const app = express();
-app.use("/api/", dates_router);
+app.use("/api/", cors(), dates_router);
 
 // app.listen(3000);
 const handler = serverless(app);

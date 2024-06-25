53cc005d393e5c45ae381cce30e918cf9568fd14
0binny0 <binnyhh@protonmail.com>
2024-06-24 23:10:12 -0700

    removed cors() import statement

--- server/netlify/functions/api/index.js
+++ server/netlify/functions/api/index.js
@@ -1,12 +1,11 @@
 
 import express from "express";
-const cors = require("cors");
 import serverless from "serverless-http";
 
 import {dates_router} from "./date.js";
 
 const app = express();
-app.use("/api/", cors(), dates_router);
+app.use("/api/", dates_router);
 
 // app.listen(3000);
 const handler = serverless(app);

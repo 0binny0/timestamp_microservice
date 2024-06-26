524de729db6983ae15ee8322c356a7274146a450
0binny0 <binnyhh@protonmail.com>
2024-06-26 09:54:07 -0700

    commented out mock service worker browser implementation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,21 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
+import {BrowserRouter} from "react-router-dom";
 
-  const { worker } = await import('./test/mocks/browser.js')
+// async function enableMocking() {
+//   if (process.env.NODE_ENV !== 'development') {
+//     return
+//   }
+//
+//   const { worker } = await import('./test/mocks/browser.js')
+//
+//
+//   return worker.start()
+// }
 
+// enableMocking().then(() => {
+//     ReactDOM.createRoot(document.getElementById('root')).render(
+//         <React.StrictMode>
+//             <BrowserRouter>
+//                 <App />
+//             </BrowserRouter>
+//         </React.StrictMode>
+//     )
+// })
 
-  return worker.start()
-}
-
-enableMocking().then(() => {
-    ReactDOM.createRoot(document.getElementById('root')).render(
-        <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
             <App />
-        </React.StrictMode>
-    )
-})
+        </BrowserRouter>
+    </React.StrictMode>
+)

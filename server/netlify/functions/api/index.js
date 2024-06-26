675a1d8078a2ce098b43aaa59b38f692c303a76f
0binny0 <binnyhh@protonmail.com>
2024-06-26 00:27:36 -0700

    added CORS middleware

--- server/netlify/functions/api/index.js
+++ server/netlify/functions/api/index.js
@@ -1,10 +1,13 @@
 
 import express from "express";
+import cors from "cors";
 import serverless from "serverless-http";
+
 
 import {dates_router} from "./date.js";
 
 const app = express();
+app.use(cors());
 app.use("/api/", dates_router);
 
 // app.listen(3000);
